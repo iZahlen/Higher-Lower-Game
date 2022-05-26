01020b1b88243555e76523a06addc6f22bd94af5
Zahlen <106341363+iZahlen@users.noreply.github.com>
2022-05-26 13:48:46 -0400

    Update Higher_Lower_Game.py

--- Higher_Lower_Game.py
+++ Higher_Lower_Game.py
@@ -1,19 +1,25 @@
-from ast import Break
+'''
+Zahlen
+05/25/22
+User guesses a number in a set range based on the program saying "too high" or "too low"
+Loops back around to the top when game ends if user wants to continue playing.
+'''
+
 import random
 player_name = input('Please Enter your name: ')
 print(f'\nWelcome to the higher/lower game, {player_name}!')
 x = 'y'
-while x != 'n':
+while x != 'n': #Loops to replay game.
     lower = int(input('Enter Lower Boundary: '))
     upper = int(input('Enter Upper Boundary: '))
-    while lower >= upper:
+    while lower >= upper: #loops if lower boundary is higher than upper boundary
         print('The lower boundary MUST be less than the upper boundary.')
         lower = int(input('Enter Lower Boundary: '))
         upper = int(input('Enter Upper Boundary: '))
     random_num = random.randint(lower, upper)
     guess = int(input(f'Guess a number between {lower} and {upper} '))
     done = False
-    while done == False:
+    while done == False: #Loops until user guesses correctly
         if guess > random_num:
             print('Too High!')
             guess = int(input('\nPlease guess again:'))
@@ -25,7 +31,7 @@ while x != 'n':
             done = True
 
     x = input('\nDo you want to play again? (y/n): ')
-    while x is not 'y' or 'n':
+    while x is not 'y' or 'n': #loops if input isn't yes/no(y/n)
         x = input(f"Im sorry {player_name}, I didn't quite get that. Do you want to play again? (y/n)")
    
-input('Thanks for playing! Press ENTER to exit. - Zahlbot')
+input('Thanks for playing! Press ENTER to exit. - Zahlen')
